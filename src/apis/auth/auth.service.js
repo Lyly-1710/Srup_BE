95c9a0ac3c7d3212c9e3ada04cc420f2ca054ff4
Ngo Thi Kim Ly <ntkl1710@gmail.com>
2024-06-28 02:57:16 +0700

    auth-login

---
+++ src/apis/auth/auth.service.js
@@ -0,0 +1,19 @@
+import usersModel from "../../models/users.model";
+class AuthService
+{
+    async login(username, password)
+    {
+       try {
+            const user = await usersModel.getUserByUsername(username);
+            console.log(user)
+            if(user === null)
+                return new Error("User not found")
+            if(user.password !== password)
+                return new Error("Password is incorrect")
+            return user
+       } catch (error) {
+        throw error
+       }
+    }
+}
+export default new AuthService();
