3d55aadad3089217742c25669fbf29a36187e290
Ngo Thi Kim Ly <ntkl1710@gmail.com>
2024-06-28 12:05:37 +0700

    auth-sign-token

--- src/apis/auth/auth.controller.js
+++ src/apis/auth/auth.controller.js
@@ -1,15 +1,19 @@
 import authService from "./auth.service";
+import 'dotenv/config';
+const jwt = require('jsonwebtoken')
+
 class AuthController{
     async login(req, res, next)
     { 
         try{
             const {username, password} = req.body;
             const user = await authService.login(username, password);
-            return res.status(200).json({
-                success: true,
-                data: user
-            });
+            const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+                expiresIn: '30s',
+              });
+              res.json({ accessToken });
         }catch(error){
+                console.log(error)
                 return res.status(500).json({
                     success: false,
                     message: "Error"
